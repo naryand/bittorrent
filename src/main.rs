7e23a20e956bb18d78bbe76435dc125405534720
quarterblue <quarterslate@gmail.com>
2021-03-21 10:20:20 -0700

    Fix pathing issues

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use bencode::Item;
 use bencode::parse;
 
 fn main() {
-    let bytes: Vec<u8> = std::fs::read("./a.torrent").expect("read error");
+    let bytes: Vec<u8> = std::fs::read("./src/a.torrent").expect("read error");
     let mut str: Vec<char> = bytes.iter().map(|b| *b as char).collect::<Vec<_>>();
     let tree: Vec<Item> = parse(&mut str);
     println!("{:?}", tree);
